301e0e9c93e68e017ef625c575888486cc82b2bd
Felipe de Oliveira Fernandes <felipe.fernandes@sydle.com>
2023-11-26 19:31:44 -0300

    Crud do produto

--- estoque/urls.py
+++ estoque/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('produtos', views.produtos, name='produtos'),
-    path('produto/novo',views.cadastrarProduto,name='cadastrarProduto'),
+    path('produto/criar',views.cadastrarProduto,name='cadastrarProduto'),
+    path('produto/editar/<id>',views.editarProduto,name='editarProduto'),
+    path('produto/deletar/<id>',views.deletarProduto,name='deletarProduto'),
     path('pratos',views.pratos, name='pratos'),
     path('prato/novo', views.cadastrarPrato,name='cadastrarPrato')
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
+from django.contrib import messages
 from .models import Produto, Prato, PratoProduto
 
 def produtos(request):
@@ -20,7 +21,33 @@ def cadastrarProduto(request):
         quantidade = request.POST.get('quantidade')
         produto = Produto.objects.create(nome=nome,data_de_ultima_compra=data_de_ultima_compra,quantidade=quantidade)
         produto.save()
+        messages.success(request, 'Produto criado com sucesso')
         return redirect('produtos')
+    
+def editarProduto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == "GET":
+        template = loader.get_template('editarProduto.html')
+        context = {
+            'produto': produto,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        nome = request.POST.get('nome')
+        data_de_ultima_compra = request.POST.get('data_de_ultima_compra')
+        quantidade = request.POST.get('quantidade')
+        produto.nome = nome
+        produto.data_de_ultima_compra = data_de_ultima_compra
+        produto.quantidade = quantidade
+        produto.save()
+        messages.success(request, 'Produto atualizado com sucesso')
+        return redirect('produtos')
+
+def deletarProduto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    produto.delete()
+    messages.success(request, 'Produto deletado com sucesso')
+    return redirect('produtos')
 
 def pratos(request):
     pratos = Prato.objects.all()
